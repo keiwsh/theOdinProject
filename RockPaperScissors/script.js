72285e7cee6110400358e314151a18665068c609
Kei <71589461+keiwsh@users.noreply.github.com>
2024-03-16 22:37:37 +0100

    new function called playRound

--- RockPaperScissors/script.js
+++ RockPaperScissors/script.js
@@ -2,12 +2,35 @@ function getComputerChoice() {
   const choices = ["Rock", "Paper", "Scissors"];
   const randomDraw = Math.floor(Math.random() * choices.length);
   const computerChoice = choices[randomDraw];
- // console.log(computerChoice);
+  // console.log(computerChoice);
   return computerChoice;
 }
 
 // const computerChoice = getComputerChoice();
 
 function playRound(playerSelection, computerSelection) {
-    
+  const winningScenario = {
+    rock: "scissors",
+    paper: "rock",
+    scissors: "paper",
+  };
+
+  playerSelection = playerSelection.toLowerCase();
+
+  // Tie between playerSelection and computerSelection
+  if (playerSelection === computerSelection) {
+    return "It's a tie! " + playerSelection + " ties with " + computerSelection;
+  }
+
+  // WinningScenarios
+  if (winningScenario[playerSelection] === computerSelection) {
+    return "You Win! " + playerSelection + " beats " + computerSelection;
+  } else {
+    return "You Lose! " + computerSelection + " beats " + playerSelection;
+  }
 }
+
+let playerSelection = prompt("Enter your choice (Rock, Paper or Scissors):");
+const computerSelection = getComputerChoice();
+const result = playRound(playerSelection, computerSelection);
+console.log(result);
